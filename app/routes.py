eec2fd54079f94ff597ad084c874449dc0d5bd6e
aroundy <gam_wannita@hotmail.com>
2018-04-12 23:39:15 +0700

    Add theme and login page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from flask import render_template
 from app import app
 
-@app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title='Home')
+
+@app.route("/")
+@app.route("/login")
+def login():
+    return render_template("login.html", title='Login')
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html", title='Dashboard')
