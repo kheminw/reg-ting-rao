02dc1e2ef059dfc3c7bf354e9a8b22314f053d08
Meatasit Karakate <khem_inw@protonmail.ch>
2018-04-14 23:24:48 +0700

    add flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_login import LoginManager
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
-from app import app
+from app import app, login_manager
+
 
 @app.route("/index")
 def index():
