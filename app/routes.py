1f619fc16bb025141506dc5f0f290380065252cc
aroundy <gam_wannita@hotmail.com>
2018-04-16 20:34:25 +0700

    Add regiter/profile/grade page

--- app/routes.py
+++ app/routes.py
@@ -12,5 +12,33 @@ def login():
     return render_template("login.html", title='Login')
 
 @app.route("/main")
-def dashboard():
+def main():
     return render_template("main.html", title='Dashboard')
+
+@app.route("/schedule")
+def schedule():
+    return render_template("schedule.html", title='Dashboard')
+
+@app.route("/grade")
+def grade():
+    return render_template("grade.html", title='Dashboard')
+
+@app.route("/register")
+def error():
+    return render_template("register.html", title='Dashboard')
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", title='Dashboard')
+
+@app.route("/addcourse")
+def addcourse():
+    return render_template("add-eliminate.html", title='Dashboard')
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html", title='Dashboard')
+
+@app.route("/tuition")
+def tuition():
+    return render_template("tuition.html", title='Dashboard')
